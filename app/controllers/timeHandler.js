7eff4945c777585257922c317bd522a776744d04
Manny Vind <mannyv+fcc@gmail.com>
2017-04-30 18:14:13 +0000

    convert date string to json object

---
+++ app/controllers/timeHandler.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function timeHandler (req, res) {
+    var time = new Date(req.params.date)
+    
+    if(isNaN(time))
+        time = new Date(parseInt(req.params.date))
+    console.log(time)
+    var result = {};
+    if(isNaN(time)) {
+        result.unix = null
+        result.natural = null;
+    }
+    else  {
+        result.unix = time.getTime()/1000|0;
+        result.natural = time.toLocaleString("en-us", {month: "long", day: "numeric", year: "numeric"})
+    }    
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result))
+}
+
+module.exports = timeHandler;
