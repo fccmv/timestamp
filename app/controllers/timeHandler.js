51208c8ddebe0f21ed26198124a555c8eba758d9
Manny Vind <mannyv+fcc@gmail.com>
2017-04-30 18:41:18 +0000

    handle unixtime requests correctly

--- app/controllers/timeHandler.js
+++ app/controllers/timeHandler.js
@@ -4,7 +4,7 @@ function timeHandler (req, res) {
     var time = new Date(req.params.date)
     
     if(isNaN(time))
-        time = new Date(parseInt(req.params.date))
+        time = new Date(parseInt(req.params.date)*1000)
 
     var result = {};
     if(isNaN(time)) {
